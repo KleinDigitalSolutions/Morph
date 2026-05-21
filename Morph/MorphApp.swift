5ef65af57ea7ccee6f67a95930fa729fea8d9833
bucci369 <danyal64@hotmail.com>
2026-05-21 09:16:49 +0200

    Initial Commit

---
+++ Morph/MorphApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorphApp.swift
+//  Morph
+//
+//  Created by Özgür Azap on 21.05.26.
+//
+
+import SwiftUI
+
+@main
+struct MorphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MorphTests/MorphTests.swift
@@ -0,0 +1,19 @@
+//
+//  MorphTests.swift
+//  MorphTests
+//
+//  Created by Özgür Azap on 21.05.26.
+//
+
+import Testing
+@testable import Morph
+
+struct MorphTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
